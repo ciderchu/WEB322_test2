e28b2c6c83da977511517440b205426c04b685c4
Sin Kau Chu <skchu3@myseneca.ca>
2024-10-04 19:30:35 -0400

    adjusted the folder structure

---
+++ MyServer/public/js/artists.js
@@ -0,0 +1,62 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  /* TODO */
+  {
+    artistId: "AID-001",
+    name: "LiSA",
+    urls: [
+      { url: "https://www.lxixsxa.com/", name: "Website" },
+      {
+        url: "https://en.wikipedia.org/wiki/LiSA_(Japanese_musician,_born_1987)",
+        name: "Wikipedia"
+      },
+      { url: "https://www.instagram.com/xlisa_olivex/", name: "Instagram" },
+      { url: "https://www.youtube.com/channel/UCqEfdEvLG5oQWNYlDQrGlKw", name: "YouTube" }
+    ]
+  },
+  {
+    artistId: "AID-002",
+    name: "Ado",
+    urls: [
+      { url: "https://music.apple.com/us/artist/ado/1492604670", name: "Website" },
+      { url: "https://en.wikipedia.org/wiki/Ado_(singer)", name: "Wikipedia" },
+      { url: "https://www.instagram.com/ado1024imokenp/", name: "Instagram" },
+      { url: "https://www.youtube.com/@Ado1024", name: "YouTube" }
+    ]
+  },
+  {
+    artistId: "AID-003",
+    name: "Kenshi Yonezu",
+    urls: [
+      { url: "https://reissuerecords.net/artist/米津玄師", name: "Website" },
+      { url: "https://en.wikipedia.org/wiki/Kenshi_Yonezu", name: "Wikipedia" },
+      { url: "https://www.instagram.com/hachi_08/", name: "Instagram" },
+      { url: "https://www.youtube.com/channel/UCUCeZaZeJbEYAAzvMgrKOPQ", name: "YouTube" }
+    ]
+  },
+  {
+    artistId: "AID-004",
+    name: "YOASOBI",
+    urls: [
+      { url: "https://www.yoasobi-music.jp/", name: "Website" },
+      { url: "https://en.wikipedia.org/wiki/Yoasobi", name: "Wikipedia" },
+      { url: "https://www.instagram.com/yoasobi_staff_/?hl=en", name: "Instagram" },
+      { url: "https://www.youtube.com/@Ayase_YOASOBI", name: "YouTube" }
+    ]
+  },
+  {
+    artistId: "AID-005",
+    name: "milet",
+    urls: [
+      { url: "https://www.milet.jp/", name: "Website" },
+      { url: "https://en.wikipedia.org/wiki/Milet_(singer)", name: "Wikipedia" },
+      { url: "https://www.instagram.com/milet_music/", name: "Instagram" },
+      { url: "https://www.youtube.com/@miletOfficialYouTubeChannel", name: "YouTube" }
+    ]
+  }
+];
